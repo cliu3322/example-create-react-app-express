763a34c5266888c7b193c871ceaaa23eba89e29b
C. Liu <cliu3322@hawaii.edu>
2019-06-08 13:00:24 -1000

    test

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const { spawn, exec } = require('child_process');
 const socketio = require('socket.io')(4000);
+socketio.origins(['*.*']);
+
 const fileUpload = require('express-fileupload');
 
 const app = express();
