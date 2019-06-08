28aa41fcabd391be1b3dccafed001f64efe445e5
C. Liu <cliu3322@hawaii.edu>
2019-06-08 12:54:52 -1000

    return

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
+
 
 app.use(fileUpload());
 // API calls
