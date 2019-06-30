caee2d2ac60f87c554fd909782b90a6d82a3db2c
C. Liu <cliu3322@hawaii.edu>
2019-06-30 21:36:56 +0800

    omg

--- server.js
+++ server.js
@@ -73,6 +73,15 @@ app.post('/api/world', (req, res) => {
 
   if(!result.includes(req.body.project)) {
     fs.mkdirSync(directorystr+req.body.project);
+    if(!getDirectories(directorystr+req.body.project).includes('pipeline')) {
+      fs.mkdirSync(directorystr+req.body.project+'/pipeline');
+    }
+  }
+
+
+
+  if(!result.includes(req.body.project)) {
+    fs.mkdirSync(directorystr+req.body.project);
     fs.mkdirSync(directorystr+req.body.project+'/pipeline');
   }
   console.log(`${directorystr+req.body.project}/pipeline/uploads/${req.files.file.name}`)
