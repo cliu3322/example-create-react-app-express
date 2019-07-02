c465b558407cdb52f504abf820feb06972e7040a
C. Liu <cliu3322@hawaii.edu>
2019-07-02 19:24:01 +0700

    str

--- server.js
+++ server.js
@@ -274,20 +274,21 @@ app.post('/api/report', (req, res) => {
         fs.mkdirSync(directorystr+req.body.project+'/pipeline/goleft');
       }
 
-      str = 'pwd'
+      str = 'goleft indexcov --d '+ directorystr+req.body.project+'/pipeline/goleft'
       if (req.body.reportmethod.includes('bismarkreport')) {
         fs.copyFile(directorystr+req.body.project+'/pipeline/bismarkResult/test.filter.bam',  directorystr+req.body.project+'/pipeline/goleft/'+'bismark.bam', (err) => {
           if (err) throw err;
           console.log('source.txt was copied to destination.txt');
+          str += ' bismark.bam';
         });
-        //str += ' && ' + 'goleft indexcov --d ' + directorystr+req.body.project+'/pipeline/goleft ' + directorystr+req.body.project+'/pipeline/bismarkResult/test.filter.bam';
+
       }
       if (req.body.reportmethod.includes('bwareport')) {
-        fs.copyFile(directorystr+req.body.project+'/pipeline/bwaResult/test.sorted.bam',  directorystr+req.body.project+'/pipeline/goleft/'+'bwa.bam', (err) => {
+        fs.copyFile(directorystr+req.body.project+'/pipeline/bwaResult/test.sort.bam',  directorystr+req.body.project+'/pipeline/goleft/'+'bwa.bam', (err) => {
           if (err) throw err;
           console.log('source.txt was copied to destination.txt');
         });
-        //str += ' && ' + 'goleft indexcov --d ' + directorystr+req.body.project+'/pipeline/goleft ' + directorystr+req.body.project+'/pipeline/bwaResult/test.filter.bam';
+        str += ' bwa.bam';
       }
 
       break;
